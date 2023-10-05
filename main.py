4fd4c6758b76759516e0075b79ea9f5e72cb8c85
Yasir <DPSA00609@mypaceschool.com>
2023-10-05 13:55:22 +0400

    Returning the right data for temperature

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 ## Importing
 from flask import Flask, render_template
+import pandas as pd
+import matplotlib as plot
 
+## Initiating the app
 app = Flask(__name__)
+
 
 
 @app.route("/")
@@ -11,7 +15,11 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
-    temperature = 23
+    ## Reading the data
+    filename = f"data\\TG_STAID{str(station).zfill(6)}.txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    temperature = df.loc[df["    DATE"] == date]["   TG"].squeeze() / 10
+    ## Showing the API
     return {"station": station,
             "date": date,
             "temperature": temperature}
