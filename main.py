1b5c031860633ee94d0d7967bf8d220f63376fe4
Yasir <DPSA00609@mypaceschool.com>
2023-10-05 16:18:38 +0400

    Adding the yearly data url endpoint and the url endpoint of the API which links to the data of all the dates of a station

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 ## Importing
 from flask import Flask, render_template
 import pandas as pd
-import matplotlib as plot
 
 ## Initiating the app
 app = Flask(__name__)
@@ -16,17 +15,40 @@ def home():
 
 
 @app.route("/api/<station>/<date>")
-def about(station, date):
+def data(station, date):
     ## Reading the data
     filename = f"data\\TG_STAID{str(station).zfill(6)}.txt"
     df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
     temperature = df.loc[df["    DATE"] == date]["   TG"].squeeze() / 10
-
     ## Showing the API
     return {"station": station,
             "date": date,
             "temperature": temperature}
 
+@app.route("/api/1/<station>")
+def all_station_data(station):
+    ## Reading the data
+    filename = f"data\\TG_STAID{str(station).zfill(6)}.txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    df = df.loc[df["   TG"] != -9999]
+    df["   TG"] = df["   TG"] / 10
+    df = df[["    DATE", "   TG"]]
+    ## Generating the API
+    result_dict = df.to_dict(orient="records")
+    ## Showing the API
+    return result_dict
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def all_station_data_annual(station, year):
+    ## Reading the data
+    filename = f"data\\TG_STAID{str(station).zfill(6)}.txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df = df.loc[df["   TG"] != -9999]
+    df["   TG"] = df["   TG"] / 10
+    df["    DATE"] = df["    DATE"].astype(str)
+    result_dict = df[df["    DATE"].str.startswith(str(year))].to_dict(
+        orient="records")
+    return result_dict
 
 if __name__ == "__main__":
     app.run(debug=True)
