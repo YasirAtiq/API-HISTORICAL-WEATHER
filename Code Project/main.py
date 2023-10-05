75634b8de27abffaa16d020cfe421e11486a1f78
Yasir <DPSA00609@mypaceschool.com>
2023-10-05 15:02:58 +0400

    Code Project-2

--- Code Project/main.py
+++ Code Project/main.py
@@ -1,5 +1,7 @@
 ## Importing
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
+import pandas as pd
 
 app = Flask("Website")
 
@@ -9,12 +11,13 @@ def home():
     return render_template('home.html')
 
 
-@app.route("/api/v1/<word>")
+@app.route("/api/<word>")
 def about(word):
-    temperature = 23
-    return {"definition": word.upper(),
-            "word": word}
+    global definition
+    df = pd.read_csv("dictionary.csv")
+    definition = df.loc[df["word"] == word]["definition"].squeeze()
+    return {"definition": definition, "word": word}
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=1234)
